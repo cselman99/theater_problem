d099ca89f25103b6f849989f2b4e3120f53d9f80
carterselman99@gmail.com <carterselman99@gmail.com>
2021-03-23 23:06:44 -0700

    Generalized code for different sized theaters

---
+++ README.txt
@@ -0,0 +1,12 @@
+ASSUMPTIONS:
+* Party size is limited to 20 individuals (an entire row)
+* individuals would prefer seating near the back rather than the front
+* 
+
+BUILD INSTRUCTIONS:
+
+
+RUN INSTRUCTIONS:
+python3 theater.py path
+* NOTE: path argument is the destination of the reservations file *
+View "output.txt" for listed results and terminal for grid view of seating chart
--- theater.py
+++ theater.py
@@ -23,9 +23,18 @@
 # R002 F5
 # R003 J1, J2, J3, J4
 
+# -------------------------------------------------------------------- #
 import sys
+import string
 
-# sys.argv
+# ! Global 2D map of the theater (10 x 20)
+ROWS = 10 # Size of ROWS limited by size of alphabet
+SEATS = 20
+
+mapTheater = [[0]*SEATS for s in range(ROWS)]
+rows = string.ascii_uppercase[:ROWS]
+
+TICKET_PRICE = 20
 
 def run(inFile):
     with open(inFile, 'r') as f:
@@ -38,36 +47,89 @@ def run(inFile):
         print('Invalid input file format')
     
     processed = []
-    for p in pairs:
-        processed.append(getSeats(p))
+    total = 0
 
+    for p in pairs:
+        seats = getSeats(p)
+        if seats is not None:
+            processed.append(seats)
+            total += int(p[1])
+        else:
+            processed.append([p[0], 'None'])
+            print('Could not find seats for reservation ' + p[0])
 
     with open('output.txt', 'w') as f:
         for p in processed:
             f.write(p[0] + ' ' + p[1] + '\n')
 
 
-#      [[ Screen ]]
-# A ssssssssssssssssssss
-# B ssssssssssssssssssss
-# C ssssssssssssssssssss
-# D ssssssssssssssssssss
-# E ssssssssssssssssssss
-# F ssssssssssssssssssss
-# G ssssssssssssssssssss
-# H ssssssssssssssssssss
-# I ssssssssssssssssssss
-# J ssssssssssssssssssss
+    print('')
+    print('Total seats sold: ' + str(total) + '/200')
+    print('Total revenue: $' + str(total * TICKET_PRICE))
+
+    CRED = '\033[92m'
+    BOLD = '\033[1m'
+    CEND = '\033[0m'
+
+    print('')
+    print(' ' * (SEATS - 3) + '[[ SCREEN ]]')
+    print('    ' + '-' * (SEATS * 2 - 1))
+    for c, i in enumerate(mapTheater):
+        print(BOLD + str(rows[c]) + CEND, end='   ')
+        for s in i:
+            if s == 1:
+                print(CRED + BOLD + str(s) + ' ' + CEND, end='')
+            else:
+                print(str(s) + ' ', end='')
+        print('')
+
 
 
 def getSeats(pair):
-    orderNumber = pair[0]
-    seats = pair[1]
-    return pair
+    # ! Stategy:
+    # * Check in reverse order (back to front for available rows - skip odd rows to allow for 1 row buffer)
+    # * Starting from the beginning of that row, check for next available seat (space by 3's)
+    
+    num = int(pair[1])
+    for x in range(len(mapTheater)-1, -1, -2):
+        y = 0
+        
+        while y < len(mapTheater[x])-num+1:
+            counter = 0
+            if mapTheater[x][y] == 0:
+
+                err = False
+                while(counter < num):
+                    if mapTheater[x][y + counter] == 1:
+                        err = True
+                        break
+                    counter += 1
+                    
+                if not err:
+                    counter = 0
+                    seatList = ''
+
+                    while(counter < num):
+                        mapTheater[x][y+counter] = 1
+                        seatList += str(rows[x]) + str(y+counter+1) + ', '
+                        counter += 1
+                    return (pair[0], seatList[:-2])
+                else:
+                    print('here')
+                    y = y + counter + 3
+            else:
+                while y + counter < SEATS and mapTheater[x][y + counter] == 1:
+                    counter += 1
+                y = y + counter + 3
+                    
+                    
+    return None
+
 
 
 
 try:
+    #print(mapTheater)
     run(sys.argv[1])
 except Exception as e:
     print(e)
